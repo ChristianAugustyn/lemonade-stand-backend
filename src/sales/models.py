d0e2a84790ac2a33f3dff936a8ef77b37f0bb58e
Christian <chrisaug@rogers.com>
2020-05-15 12:40:37 -0400

    added sales model

--- src/sales/models.py
+++ src/sales/models.py
@@ -1,13 +1,19 @@
 from django.db import models
 
-from product.models import Product
+from products.models import Product
 from staff.models import StaffMember
 # Create your models here.
 class Sale(models.Model):
-    PRODUCTS = [
+    PRODUCTS = map( #not needed
+        (lambda model: (model.id, model.name)), 
+        Product.objects.all()
+    )
 
-    ]
+    STAFF = map( #not needed
+        (lambda model: (model.id, ' '.join([model.first_name, model.last_name]))),
+        StaffMember.objects.all()
+    )
 
-    STAFF = [
-        
-    ]
+    date = models.DateTimeField(auto_now=False, auto_now_add=True)
+    item = models.ForeignKey(Product, on_delete=models.CASCADE)
+    staff_member = models.ForeignKey(StaffMember, on_delete=models.CASCADE)
